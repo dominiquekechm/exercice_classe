f091a5e69c184a441c337f602f2d95b8ae8f52ce
dominiquekechm <78029516+dominiquekechm@users.noreply.github.com>
2021-02-02 10:43:10 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+MATTEO DK
+GR 407
+CLASS
+"""
+
+from dataclasses import dataclass
+import random
+
+
+class NPC():
+    def __init__(self, ):
+        self.con
+        self.dex
+        self.cha
+        self.intel
+        self.wis
+        self.strenght
+        self.name = ""
+        self.race = ""
+        self.specifies = ""
+        self.protectuion = ""
+        self.ac = random.randint(1, 13)
+        self.hp = random.randint(1, 21)
+
+
+
+
